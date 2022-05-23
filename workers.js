c2b5136329bc6d6a52bae613e62b07cc1938015e
adilzada <100288857+adilzada@users.noreply.github.com>
2022-05-23 04:59:20 +0400

    second load

---
+++ workers.js
@@ -0,0 +1,9 @@
+const express = require('express');
+const app = express();
+const fs= require('fs');
+fs.readFile('./workers.json', 'utf8', (err, data) => {
+    app.get('/', function (req, res) {
+        res.send(data);
+      });
+
+})
