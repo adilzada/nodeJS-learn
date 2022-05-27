390ecd16df32817336f9e9eeffe81b192ea257e1
adilzada <100288857+adilzada@users.noreply.github.com>
2022-05-27 17:41:33 +0400

    blabla

--- bankCard.js
+++ bankCard.js
@@ -3,17 +3,17 @@ const app = express();
 const users = [  
 { "id":4123 ,"cardNumber": 112233445566 },
 { "id":4321 ,"cardNumber": 665544332211 },
+{ "id":2134 ,"cardNumber": 123456789012},
 { "id":1234 ,"cardNumber": 123456789012},
-{ "id":1234 ,"cardNumber": 123456789012},
-{"id":1234 ,"cardNumber": 123456789012}
+{"id":1243 ,"cardNumber": 123456789012}
 ];
-app.get('/user', function (req, res) {
-  res.send(JSON.stringify(users.find((user) => user.id == req.params.id)));
+app.get('/users', function (req, res) {
+  res.send(JSON.stringify(users));
 });
-app.get('/user/:id', function (req, res) {
-  res.send(JSON.stringify(users.find((user) => user.id == req.params.id)));
+app.get('/users/:id', function (req, res) {
+  res.send(JSON.stringify(users.find((users) => users.id == req.params.id)));
 });
-app.listen(3006, function () {
+app.listen(3007, function () {
     console.log('Example app listening on port 3006!');
   });
 
